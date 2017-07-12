d82fa75a477863e63f1b3cb881685aee0e6b6f81
Philipp Koch <philipp.koch@th-nuernberg.de>
2017-07-12 22:41:19 +0200

    Added schwengel picture. Added first test for drawing the play area.

--- src/WidgetExample.cpp
+++ src/WidgetExample.cpp
@@ -31,18 +31,37 @@ WidgetExample::~WidgetExample()
 void WidgetExample::paintEvent(QPaintEvent* event)
 {
   QPainter painter(this);
-  QString path = QDir::currentPath() + "/cutees.jpg";
+  QString path = QDir::currentPath() + "/images/active.jpg";
   QImage image(path);
+  path = QDir::currentPath() + "/images/schwengel.jpg";
+  QImage schwengel(path);
 
-  painter.drawImage(this->rect(), image, image.rect());
-  QBrush brush(Qt::SolidPattern);
-  brush.setColor(Qt::black);
-  painter.setBrush(brush);
-  QPen pen(Qt::SolidLine);
-  pen.setColor(Qt::black);
-  QFont serifFont("Times", 50, QFont::Bold);
-  painter.setFont(serifFont);
-  painter.drawText(this->rect().center() - QPoint(this->width() / 3, this->height() / 3), "Cuteeees");
+  for(unsigned int i = 0; i < 10; i++)
+  {
+    for(unsigned int j = 0; j < 10; j++)
+    {
+      QRect localRect = image.rect();
+      localRect.moveTopLeft(QPoint(i * image.height(), j * image.width()));
+      painter.drawImage(localRect, image, image.rect());
+    }
+  }
+  QRect localRect = image.rect();
+        localRect.moveTopLeft(QPoint(3 * image.height(), 2 * image.width()));
+        painter.drawImage(localRect, schwengel, schwengel.rect());
+        localRect = image.rect();
+        localRect.moveTopLeft(QPoint(7 * image.height(), 9 * image.width()));
+        painter.drawImage(localRect, schwengel, schwengel.rect());
+
+
+  //painter.drawImage(this->rect(), image, image.rect());
+//  QBrush brush(Qt::SolidPattern);
+//  brush.setColor(Qt::black);
+//  painter.setBrush(brush);
+//  QPen pen(Qt::SolidLine);
+//  pen.setColor(Qt::black);
+//  QFont serifFont("Times", this->height() / 10, QFont::Bold);
+//  painter.setFont(serifFont);
+//  painter.drawText(this->rect().center() - QPoint(this->width() / 3, this->height() / 3), "Cuteeees");
 }
 
 } /* namespace ohm_railway */
