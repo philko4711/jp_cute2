6a11a46aa78771c33d1addbb9b95aa814a58dd40
Philipp Koch <philipp.koch@th-nuernberg.de>
2017-07-15 21:57:51 +0200

    Added the necessary wrap class for the tutorial .ui. Added the build calls to cmake.

---
+++ src/TutMenu.cpp
@@ -0,0 +1,20 @@
+/*
+ * TutMenu.cpp
+ *
+ *  Created on: Jul 15, 2017
+ *      Author: phil
+ */
+
+#include "TutMenu.h"
+
+TutMenu::TutMenu()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+TutMenu::~TutMenu()
+{
+  // TODO Auto-generated destructor stub
+}
+
---
+++ src/TutMenu.h
@@ -0,0 +1,23 @@
+/*
+ * TutMenu.h
+ *
+ *  Created on: Jul 15, 2017
+ *      Author: phil
+ */
+
+#ifndef SRC_TUTMENU_H_
+#define SRC_TUTMENU_H_
+
+#include <QWidget>
+
+#include "ui_tut.h"
+
+class TutMenu: public QWidget
+{
+  Q_OBJECT
+public:
+  TutMenu();
+  virtual ~TutMenu();
+};
+
+#endif /* SRC_TUTMENU_H_ */
